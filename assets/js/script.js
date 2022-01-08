6fb1afcfd0809a2759f95cb6190df4fb53bcd54a
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-08 12:14:29 +0000

    solved

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,18 @@ var signUpButton = document.querySelector("#sign-up");
 signUpButton.addEventListener("click", function(event) {
   event.preventDefault();
   
-  // TODO: Create user object from submissio
+  // TODO: Create user object from submission
+  const user = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    email: emailInput.value,
+    password: password.value,
+  };
+  
+  console.log(user);
+
 
   // TODO: Set new submission to local storage 
+  localStorage.setItem('lastUserDetails', JSON.stringify(user));
   
 });
